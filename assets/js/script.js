72e4e33728bc33c127c0eacc9548518f17ab8af2
Nicolas <nico2che@gmail.com>
2017-10-24 16:57:30 -0400

    Add js file and script tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
 //
 (function(){
+    var page = /\/(.+).html/.exec(window.location.pathname);
+    page = page[1] ? page[1].replace('-', '') : "index";
+    if(window[page])
+        window[page]();
 })()
+
+function index() {
+    console.log('index')
+}
+
+function products() {
+    console.log('products')
+}
+
+function product() {
+    console.log('product')
+}
+
+function order() {
+    console.log('order')
+}
+
+function shoppingcart() {
+    console.log('shoppingcart')
+}
