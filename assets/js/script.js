fc77e222490040d65cde9326b04105263ec062c4
Nicolas <nico2che@gmail.com>
2017-10-26 01:32:09 -0400

    new pattern, shopping-cart tests fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,79 @@
 "use strict";
 
-(function(){
-    var page = /\/(.+).html/.exec(window.location.pathname);
-    page = page[1] ? page[1].replace('-', '') : "index";
-    if(window[page])
-        window[page]();
-    updateCart();
+var Storage = (function(){
+    
+    var products = null;
+    
+    /**
+     * Return product count
+     */
+    function length() {
+        // if(!products)
+        //     get();
+        return Object.keys(products).length ? Object.values(products).reduce((p1, p2) => p1 + p2) : 0;
+    }
+
+    /**
+     * Get all articles
+     */
+    function get() {
+        if (products) {
+            return products;
+        } else {
+            var cart = localStorage.getItem('cart');
+            products = cart ? JSON.parse(cart) : {};
+            updateCounter();
+            return products;
+        }
+    }
+
+    /**
+     * Add or remove specific article, x time
+     * @param {int} number 
+     * @param {int} article 
+     */
+    function set(number, article) {
+        number = parseInt(number);
+        products[article] = products[article] ? products[article] + number : number;
+        updateCounter();
+        localStorage.setItem('cart', JSON.stringify(products));
+        return this;
+    }
+    
+    /**
+     * Remove all specific article
+     * @param {int} article 
+     */
+    function remove(article) {
+        delete products[article]; // Remove article
+        updateCounter();
+        localStorage.setItem('cart', JSON.stringify(products)); // Save
+        return this;
+    }
+        
+    /**
+     * Remove all articles
+     */
+    function removeAll() {
+        products = {};
+        updateCounter();
+        localStorage.removeItem('cart');
+        return this;
+    }
+
+    function updateCounter() {
+        var count = length();
+        count > 0 ? $('.shopping-cart .count').html(count).show() : $('.shopping-cart .count').hide();
+    }
+
+    return { get, set, remove, removeAll, length }
 })();
 
-function updateCart(number, article) {
-    // Get cart items
-    var cart = localStorage.getItem('cart');
-    cart = cart ? JSON.parse(cart) : {};
-    // Concat with new items
-    if(article && number)
-        cart[article] = cart[article] ? cart[article] + parseInt(number) : parseInt(number);
-    else if (article && number === 0)
-        delete cart[article];
-    // Store new data
-    localStorage.setItem('cart', JSON.stringify(cart));
-    // Update menu cart
-    var count = Object.keys(cart).length ? Object.values(cart).reduce((p1, p2) => p1 + p2) : 0;
-    if(count > 0)
-        $('.shopping-cart .count').html(count).show();
-    else
-        $('.shopping-cart .count').hide();
-}
+var View = (function(){
+
+    function formatPrice(price) {
+        return price.toFixed(2).toString().replace('.', ',');
+    }
+
+    return { formatPrice }
+})();
--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -1,73 +1,101 @@
 "use strict";
 
-function shoppingcart() {
-    var cart = localStorage.getItem('cart');
-    if(cart) {
-        cart = JSON.parse(cart);
-        $.get('data/products.json', function(articles) {
-            articles.filter(function(article){
-                return cart[article.id];
-            }).sort(function(p1, p2){
+var ShoppingCart = (function(articles){
+
+    function init() {
+        if (Storage.length() === 0) {
+            hideTable();
+        } else {
+            loadTable();
+        }
+    }
+
+    function loadTable() {
+        $.get('data/products.json', function(data) {
+            data.filter(function(article){ // Filter only ou articles
+                return articles[article.id];
+            }).sort(function(p1, p2){ // Sort them alpha A-Z
                 return p1.name.toLowerCase() > p2.name.toLowerCase() ? 1 : -1;
-            }).map(function(article) {
-                var row = '<tr data-price="' + article.price + '" data-id="' + article.id + '">' +
-                            '<td><button class="remove-item-button"><i class="fa fa-remove"></i></button></td>' +
-                            '<td><a href="product.html?id=' + article.id + '">' + article.name + '</a></td>' +
-                            '<td>' + article.price + ' $</td>' +
-                            '<td>' +
-                                '<button class="remove-quantity-button"' + (cart[article.id] < 2 ? ' disabled=""' : '') +
-                                '><i class="fa fa-minus"></i></button> ' +
-                                '<span class="quantity">' + cart[article.id] + '</span> ' +
-                                '<button class="add-quantity-button"><i class="fa fa-plus"></i></button>' +
-                            '</td>' +
-                            '<td class="price">' + (article.price * cart[article.id]) + '$</td>' +
-                        '</tr>';
-                $('tbody').append(row);
+            }).map(function(article) { // And display
+                addProduct(article);
             });
-
-            $('button.remove-quantity-button, button.add-quantity-button').on('click', function(){
-                // Get id
-                var row = $(this).parent().parent();
-                var id = row.data('id');
-                var incrementer = $(this).hasClass('remove-quantity-button') ? -1 : 1;
-                // Update menu cart and storage
-                updateCart(incrementer, id);
-                // Update table
-                cart[id] += incrementer;
-                if(cart[id] < 2) {
-                    $(this).attr('disabled', 'disabled');
-                } else {
-                    row.find('.remove-quantity-button').removeAttr('disabled');
-                }
-                row.find('.quantity').html(cart[id]);
-                row.find('.price').html(parseFloat(row.data('price') * cart[id] + '$'));
-            });
-
-            $('.remove-item-button').on('click', function(){
-                if(confirm('Voulez-vous supprimer le produit du panier ?')) {
-                    var row = $(this).parent().parent();
-                    var id = row.data('id');
-                    // Update menu cart and storage
-                    updateCart(0, id);
-                    // Update table
-                    row.remove();
-                }
-            });
-            
-            $('.remove-all-items-button').on('click', function(){
-                if(confirm('Voulez-vous supprimer tous les produits du panier ?')) {
-                    // Remove all products from storage
-                    localStorage.removeItem('cart');
-                    // Update menu cart
-                    updateCart();
-                    // Update table
-                    $('tbody tr').remove();
-                }
-            });
+            updateTotal(); // Update total amount
+            // Buttons events
+            $('button.remove-quantity-button, button.add-quantity-button').on('click', changeQuantity);
+            $('.remove-item-button').on('click', removeProduct);
+            $('#remove-all-items-button').on('click', removeAllProducts);
         }, 'JSON');
     }
-}
 
-function updateTotal() {
-    //$('#total-amount')
-}
+    function addProduct(article) {
+        var row = '<tr data-price="' + article.price + '" data-id="' + article.id + '">' +
+                        '<td><button class="remove-item-button"><i class="fa fa-remove"></i></button></td>' +
+                        '<td><a href="product.html?id=' + article.id + '">' + article.name + '</a></td>' +
+                        '<td>' + View.formatPrice(article.price) + ' $</td>' +
+                        '<td>' +
+                            '<button class="remove-quantity-button"' + (articles[article.id] < 2 ? ' disabled=""' : '') +
+                            '><i class="fa fa-minus"></i></button> ' +
+                            '<span class="quantity">' + articles[article.id] + '</span> ' +
+                            '<button class="add-quantity-button"><i class="fa fa-plus"></i></button>' +
+                        '</td>' +
+                        '<td class="price">' + View.formatPrice(article.price * articles[article.id]) + ' $</td>' +
+                    '</tr>';
+        $('tbody').append(row);
+    }
+
+    function changeQuantity() {
+        var row = $(this).parent().parent();
+        var id = row.data('id'); // Get article id
+        var incrementer = $(this).hasClass('remove-quantity-button') ? -1 : 1;
+        Storage.set(incrementer, id); // Update storage
+        // Disable/Active button if necessary
+        if(articles[id] < 2) {
+            $(this).attr('disabled', 'disabled');
+        } else {
+            row.find('.remove-quantity-button').removeAttr('disabled');
+        }
+        // Update quantity & price
+        row.find('.quantity').html(articles[id]);
+        row.find('.price').html(View.formatPrice(parseFloat(row.data('price') * articles[id])) + ' $');
+        updateTotal(); // Update total amount
+    }
+
+    function removeProduct() {
+        if(confirm('Voulez-vous supprimer le produit du panier ?')) {
+            var row = $(this).parent().parent();
+            var id = row.data('id');
+            Storage.remove(id); // Update menu cart and storage
+            row.remove(); // Update table
+            if(Storage.length === 0)
+                hideTable(); // Hide table if no product anymore
+            else
+                updateTotal(); // Update total amount
+        }
+    }
+
+    function removeAllProducts() {
+        if(confirm('Voulez-vous supprimer tous les produits du panier ?')) {
+            Storage.removeAll(); // Remove all products
+            hideTable();
+        }
+    }
+    
+    function updateTotal() {
+        var totalAmount = 0;
+        $('tbody tr').each(function(){
+            var price = parseFloat($(this).data('price'));
+            totalAmount += price * articles[$(this).data('id')];
+        });
+        $('#total-amount').html(View.formatPrice(totalAmount));
+    }
+
+    function hideTable() {
+        $('table').remove();
+        $('#shopping-cart p').html('Aucun produit dans le panier.');
+    }
+
+    return { init }
+
+})(Storage.get());
+
+ShoppingCart.init();
