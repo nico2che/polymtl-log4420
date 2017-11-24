4cf9d910657115f7262fff02999e7e92665a001b
Nicolas <nico2che@gmail.com>
2017-11-23 19:42:14 -0500

    dependencies update + orders, shopping-cart api

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,19 +1,23 @@
 var express = require("express")
     , router = express.Router()
+    , _ = require('validator')
     , Order = require("../../models/Order")
 
 // GEt all orders
 router.get("/orders", function(req, res) {
-  Order.find().exec((err, orders) => {
-    if (err) res.status(400).json({"error": err})
-    res.json(orders);
+  Order.find().exec((error, orders) => {
+    if (error)
+      res.status(500).json({ error }) // Check error
+    else
+      res.json(orders);
   })
 });
 
 // Get orders id = :id
 router.get("/orders/:id", function(req, res) {
-  Order.findOne({ id: req.params.id }).exec((err, order) => {
-    if (err) res.status(400).json({"error": err})
+  const id = parseInt(req.params.id);
+  Order.findOne({ id }).exec((error, order) => {
+    if (error) res.status(500).json({ error }) // Check error
     if (order)
       res.json(order);
     else
@@ -23,25 +27,48 @@ router.get("/orders/:id", function(req, res) {
 
 // Create a order
 router.post("/orders", function(req, res) {
-  const order = new Order(req.body);
-  order.save((err, order) => {
-    if (err) res.status(400).json({"error": err})
-    res.status(201).json(order);
-  })
+  // Get all parameters sent
+  const { firstName, lastName, email, phone, products } = req.body;
+  // Check all parameters
+  if (firstName && lastName && _.isEmail(email) && _.isMobilePhone(phone, 'any')
+        && typeof products === 'object' && products.length > 0) {
+    // Get the last order
+    Order.findOne().sort({ id: -1 }).exec((error, lastOrder) => {
+      if (error) res.status(500).json({ error }) // Check error
+      // Define new ID
+      const id = lastOrder ? ++lastOrder.id : 1;
+      // Create new order with all info and new id
+      const order = new Order({ ...req.body, id });
+      order.save((error, order) => {
+        if (error) res.status(500).json({ error })
+        res.status(201).json(order); // Save and send
+      })
+    })
+  } else {
+    res.status(400).send()
+  }
 });
 
 // Delete order id = :id
 router.delete("/orders/:id", function(req, res) {
-  order.remove({ id: req.params.id }, (err) => {
-    if (err) res.status(400).json({"error": err})
-    res.status(204).send();
+  const id = parseInt(req.params.id);
+  Order.findOne({ id }).exec((error, orderToRemove) => {
+    if (error) res.status(500).json({ error }) // Check error
+    if (orderToRemove) { // Ok, this order exists, remove it
+      Order.remove({ id }, (error) => {
+        if (error) res.status(500).json({ error })
+        res.status(204).send();
+      })
+    } else {
+      res.status(404).send(); // No order with this :id
+    }
   })
 });
 
 // Delete all orders
 router.delete("/orders", function(req, res) {
-  order.remove({}, (err) => {
-    if (err) res.status(400).json({"error": err})
+  Order.remove({}, (error) => {
+    if (error) res.status(500).json({ error })
     res.status(204).send();
   })
 });
--- routes/api/shopping-cart.js
+++ routes/api/shopping-cart.js
@@ -1,5 +1,4 @@
 var express = require("express")
-    , _ = require('lodash')
     , router = express.Router();
 
 getProduct = (req, productId) => {
@@ -9,28 +8,26 @@ getProduct = (req, productId) => {
 
 // GEt all product in shopping cart
 router.get("/shopping-cart", function(req, res) {
-  if (!_.isArray(req.session.cart)) req.session.cart = [];
+  if (typeof req.session.cart !== "object") req.session.cart = [];
   res.json(req.session.cart);
 });
 
 // Get product id = :productId in shopping-cart
 router.get("/shopping-cart/:productId", function(req, res) {
-  const product = getProduct(req, _.toInteger(req.params.productId));
+  const product = getProduct(req, parseInt(req.params.productId));
   res.status(product ? 200 : 404).json(product || []);
 });
 
 // Add a product in shopping cart
 router.post("/shopping-cart/", function(req, res) {
-  const productId = _.toInteger(req.body.productId)
-        , quantity = _.toInteger(req.body.quantity);
-  console.log(quantity, productId)
+  const productId = parseInt(req.body.productId)
+        , quantity = parseInt(req.body.quantity);
   // Check sent parameters
   if (quantity && productId) {
     if (getProduct(req, productId)) // If exists, increment it
       req.session.cart = req.session.cart.map(p => p.productId === productId ? { productId, quantity: quantity + p.quantity } : p);
     else // Else, create it
       req.session.cart = (req.session.cart || []).concat({ productId, quantity })
-    console.log(req.session.cart)
     res.json(req.session.cart); // new cart
   } else {
     res.status(400).send(); // error in parameters
@@ -39,8 +36,8 @@ router.post("/shopping-cart/", function(req, res) {
 
 // Update quantity product :productId in shopping cart
 router.put("/shopping-cart/:productId", function(req, res) {
-  const productId = _.toInteger(req.params.productId)
-        , quantity = _.toInteger(req.body.quantity);
+  const productId = parseInt(req.params.productId)
+        , quantity = parseInt(req.body.quantity);
   // Check sent parameters
   if (quantity && productId) {
     if (getProduct(req, productId)) { // If exists, replace quantity
@@ -56,7 +53,7 @@ router.put("/shopping-cart/:productId", function(req, res) {
 
 // Delete product id = :productId in shopping cart
 router.delete("/shopping-cart/:productId", function(req, res) {
-  const productId = _.toInteger(req.params.productId);
+  const productId = parseInt(req.params.productId);
   // Check sent parameters
   if (getProduct(req, productId)) { // If exists, keep all other products
     req.session.cart = req.session.cart.filter(p => p.productId !== productId);
