ebab437201c0d5eec24492a46e023cad59180326
Nicolas de Chevigné <nico2che@gmail.com>
2017-12-05 19:17:33 -0500

    Product Component & HTML

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService, Product } from "../products.service";
 
 /**
  * Defines the component responsible to manage the product page.
@@ -14,14 +15,24 @@ export class ProductComponent implements OnInit {
    * Initializes a new instance of the ProductComponent class.
    *
    * @param route                   The active route.
+   * @param productService          Products API Service Injected
    */
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private productService: ProductsService) { }
+
+  product: Product;
+
+  getProduct(id: number): void {
+    this.productService
+      .getProduct(id)
+      .then(product => this.product = product);
+  }
 
   /**
    * Occurs when the component is initialized.
    */
   ngOnInit() {
     const productId = this.route.snapshot.paramMap.get('id');
+    this.getProduct(parseInt(productId));
     // TODO: Compléter la logique pour afficher le produit associé à l'identifiant spécifié (productId).
   }
 }
